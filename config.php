c2d4d2347a8b2b14a689cc78263ddf422abda817
NielsenMax <maxianielsen@gmail.com>
2019-08-04 20:35:02 -0300

    cambios en la configuracion de la base de datos para que sea igual a la del servidor de produccion

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'makeitwork');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'USERS');
+define('DB_USERNAME', 'poli_uno');
+define('DB_PASSWORD', 'poli1');
+define('DB_NAME', 'poli_cuatro');
  
 
 $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
@@ -12,5 +12,19 @@ $mysqli = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
 if ($mysqli->connect_error) {
     die('Connection error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
+$mysqli->query("
+CREATE TABLE if not exists `users` (
+    `id` int(11) NOT NULL unique primary key AUTO_INCREMENT,
+    `username` varchar(255) DEFAULT NULL,
+    `email` varchar(255) DEFAULT NULL unique key,
+    `password` varchar(255) DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+$mysqli->query("
+create table if not exists empresas(
+    id int NOT NULL PRIMARY KEY unique AUTO_INCREMENT,
+    idOwner int not null,
+    name varchar(255),
+    FOREIGN KEY (idOwner) REFERENCES users(id) 
+);");
 
 ?>
--- sql
+++ sql
@@ -4,3 +4,9 @@ create table empresas(
     name varchar(255),
     FOREIGN KEY (idOwner) REFERENCES users(id) 
 );
+create table users(
+    id int NOT NULL PRIMARY KEY unique AUTO_INCREMENT,
+    username varchar(255) not null unique SECONDARY KEY,
+    email varchar(255) not null unique SECONDARY KEY,
+    password varchar(255) not null  
+);
