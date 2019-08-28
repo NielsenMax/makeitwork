f73c85899d83115fe68ad7dc5504b96c436ff999
maxi nielsen <maxianielsen@gmail.com>
2019-08-28 09:51:14 -0300

    Corregido añadir users

--- addUser2Emp.php
+++ addUser2Emp.php
@@ -6,14 +6,16 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $res=$mysqli->query("select id from users where email='".$_POST['email']."';");
 $idUser=  mysqli_fetch_row($res);
+//echo $idUser[0];
 if($idUser){
-    $noRep = $mysqli->query("select id from usersdeempresas where idEmpresa='".$_POST['idEmp']."' and idUser='".$idUser[0]."';");
+    $noRep = $mysqli->query("select id from usersDeEmpresas where idEmpresa='".$_POST['idEmp']."' and idUser='".$idUser[0]."';");
     $noRep = mysqli_fetch_row($noRep);
+    //echo var_dump($noRep);
     if($noRep != NULL)
     {
         echo "<p>Este usuario ya se encuentra en la empresa</p>";
     }else{
-        $res2=$mysqli->query("insert into usersdeempresas (idEmpresa, idUser) values ('".$_POST["idEmp"]."','".$idUser[0]."');");
+        $res2=$mysqli->query("insert into usersDeEmpresas (idEmpresa, idUser) values ('".$_POST['idEmp']."','".$idUser[0]."');");
         if($res2)
         {
             echo "<p>Añadido con exito!</p>";
