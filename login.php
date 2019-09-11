19aa6ad1a903aed6c3eb1cb15361b7295d2f3f3d
AlejoGonzalez <alejogonzalezips@gmail.com>
2019-09-11 10:02:16 -0300

    arreglados hipervinculos

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
+    header("location: pages/welcome.php");
     exit;
 }
  
@@ -64,7 +64,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: welcome.php");
+                            header("location: pages/welcome.php");
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "La contraseña no es valida.";
