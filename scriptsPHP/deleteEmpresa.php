90a27c4f395fc43d9c90812f74fcc3bdb96ea33e
maxi nielsen <maxianielsen@gmail.com>
2019-09-25 10:12:23 -0300

    arreglado borrados

--- scriptsPHP/deleteEmpresa.php
+++ scriptsPHP/deleteEmpresa.php
@@ -5,7 +5,8 @@ require_once "../config.php";
 $r=$mysqli->query("select id from empresas where name='".$_POST["emp"]."' and idOwner='".$_POST["idOwner"]."';");
 if($data = mysqli_fetch_row($r))
 { 
-    if($mysqli->query("delete from empresas where name='".$_POST["emp"]."' and idOwner='".$_POST["idOwner"]."';")){
+    $res=$mysqli->query("delete from empresas where id=".$data[0].";");
+    if($res){
         echo "<p>Eliminada con exito</p>";
     }else{
         echo "<p>No se ha podido eliminar</p>";
