7727e95ee62318f25bf31893c1bdd370043491ca
NielsenMax <maxianielsen@gmail.com>
2019-09-27 09:41:24 -0300

    correjido script para añadir participantes a tareas

--- scriptsPHP/addUserTarea.php
+++ scriptsPHP/addUserTarea.php
@@ -1,16 +1,35 @@
+
 <?php
 
-require_once "../config.php";
+require_once "config.php";
 
 header("Content-Type: application/json; charset=UTF-8");
 
-$res=$mysqli->query("insert into userDeTareas (idUser, idTarea) 
-                values ('".$_POST["idUser"]."','".$_POST["idTarea"]."');");
-if($res){
-    
+$res=$mysqli->query("select id from users where email='".$_POST['email']."';");
+$idUser=  mysqli_fetch_row($res);
+//echo $idUser[0];
+if($idUser){
+    $noRep = $mysqli->query("select id from userDeTareas where idTarea='".$_POST['idTarea']."' and idUser='".$idUser[0]."';");
+    $noRep = mysqli_fetch_row($noRep);
+    //echo var_dump($noRep);
+    if($noRep != NULL)
+    {
+        echo "<p>Este usuario ya se encuentra en la tarea</p>";
+    }else{
+        $res2=$mysqli->query("insert into userDeTareas (idUser, idTarea) 
+                                values ('".$_POST["idUser"]."','".$idUSer[0]."');");
+        if($res2)
+        {
             echo "<p>Añadido con exito!</p>";
+        }else{
+            echo "<p>No se ha podido añadir al usuario a la Tarea:(</p>";
+            
+        }
+    }
 }else{
-    echo "<p>No se ha podido añadir :(</p>";
-    
+    echo "<p>No existe un usuario con ese email</p>";
 }
+
+
+
 ?>
