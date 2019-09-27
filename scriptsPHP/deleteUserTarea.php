267ce10c48a8a3fe95ad51b507abc5c8055a629d
NielsenMax <maxianielsen@gmail.com>
2019-09-27 10:08:26 -0300

    ajax para añadir user a tarea

---
+++ scriptsPHP/deleteUserTarea.php
@@ -0,0 +1,31 @@
+
+<?php
+
+require_once "config.php";
+
+header("Content-Type: application/json; charset=UTF-8");
+
+$res=$mysqli->query("select id from users where email='".$_POST['email']."';");
+$idUser=  mysqli_fetch_row($res);
+if($idUser){
+
+    $r=$mysqli->query("select id from userDeTareas where idUser='".$idUser[0]."' and idTarea='".$_POST['idTarea']."';");
+    if($data = mysqli_fetch_row($r))
+    { 
+        $res=$mysqli->query("delete from empresas where id=".$data[0].";");
+        if($res){
+            echo "<p>Eliminada con exito</p>";
+        }else{
+            echo "<p>No se pudo eliminar</p>";
+        }
+    }else{
+        echo "<p>No hay un usuario con ese nombre en esta tarea</p>";
+    }
+
+}else{
+    echo "<p>No existe un usuario con ese email</p>";
+}
+
+
+
+?>
