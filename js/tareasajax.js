aa22f2315cd21bb4c40d17348767ebc5cd787013
NielsenMax <maxianielsen@gmail.com>
2019-09-27 09:55:27 -0300

    js para el modal de participantes

--- js/tareasajax.js
+++ js/tareasajax.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-    //!PONER ID
-    //?hola
-    //TODO h
+    //!PONER ID del boton para añadir
     $(document).on("click", "#" , function() {
-        
+        //! Poner Id campo Email
+        email = document.getElementById("");
+        //! Poner id output
+        output = document.getElementById("");
+        $.ajax({
+            type: "POST",
+            url: "../scriptsPHP/addUserTarea.php",  
+            data: {
+                "idTarea" : email.attr('idTarea'),
+                "email": email.val()
+                },
+            dataType: "html",
+            success: function (response) {
+                output.html(response);
+            },
+            error: function(){
+                output.html("<p> Error en el servidor</p>");
+            }
+        });
     });
 
 });
