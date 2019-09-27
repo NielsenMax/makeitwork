f3f1ecdea30ed8d89a645d7cc66766343e5db27c
NielsenMax <maxianielsen@gmail.com>
2019-09-27 10:46:13 -0300

    ajax para eliminar tareas

--- js/tareasajax.js
+++ js/tareasajax.js
@@ -21,5 +21,26 @@ $(document).ready(function(){
             }
         });
     });
-
+    $(document).on("click", "#añadirpart" , function() {
+        email = document.getElementById("namepart");
+        console.log($(email).attr('idtarea'))
+      
+        output = document.getElementById("rAñadirPart");
+        $.ajax({
+            type: "POST",
+            url: "../scriptsPHP/deleteUserTarea.php",  
+            data: {
+                "idTarea" : $(email).attr('idtarea'),
+                
+                "email": $(email).val()
+                },
+            dataType: "html",
+            success: function (response) {
+                $(output).html(response);
+            },
+            error: function(){
+                $(output).html("<p> Error en el servidor</p>");
+            }
+        });
+    });
 });
