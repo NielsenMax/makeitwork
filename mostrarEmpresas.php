cbc9957692dd831c3f105084afe6659dc790bf7b
nielsenmax <maxianielsen@gmail.com>
2019-10-26 19:40:05 -0300

    añadido poder entrar como usuario de empresa

--- mostrarEmpresas.php
+++ mostrarEmpresas.php
@@ -1,44 +1,15 @@
 <?php 
-/*
-require_once "config.php";
-
-header("Content-Type: application/json; charset=UTF-8");
-
-
-$resp=$mysqli->query("select * from empresas;");
-$x= Array(Array());
-$c=0;
-while($i=$resp->fetch_row()){
-    $x[$c]=$i;
-    $c++;
-}
-echo json_encode($x);*/
-
 include("config.php");
-//mysqli_select_db("samples",$con);
 $result=$mysqli->query("select * from empresas where idOwner='".$_GET['idOwner']."';");
-
-
-
-// echo "<table border='1' >
-// <tr>
-// <td ><b>Nombre de la empresa</b></td>
-// <td ><b>Nombre de empleado</b></td>
-// <td ><b>Añadir empleados</b></td>
-//";
 
 while($data = mysqli_fetch_row($result))
 {   
-
-    //echo "<tr class=pito>";
-    //echo "<td >$data[0]</td>";
-    //echo "<td>$data[1]</td>";
-    // echo "<td >$data[2]</td>";
-    // echo "<td ><input type='text' id=email'".$data[0]."'></td>";
-    // echo "<td ><input type='button' value='agregar' class='addU' id='".$data[0]."'></td>";
-    // echo "</tr>";
     echo "<a href='empresa.php?emp=".$data[0]."' >" . $data[2] . "</a>";
 }
-echo "</table>";
-
+echo "<p style='background-color:#cccccc ' >   Mis Empleos</p>";
+$r = $mysqli->query("select * from empresas inner join usersDeEmpresas on empresas.id = usersDeEmpresas.idEmpresa where usersDeEmpresas.idUser =".$_GET['idOwner'].";");
+while($data = mysqli_fetch_row($r))
+{   
+    echo "<a href='empresa.php?emp=".$data[0]."' >" . $data[2] . "</a>";
+}
 ?>
